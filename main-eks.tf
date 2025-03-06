574d7a3f004a04a619bd25ff0022d5a01260bd9d
Myoko <myokoko1651996@gmail.com>
2025-03-06 16:16:16 +0630

    change providers.tf to terraform.tf

--- main-eks.tf
+++ main-eks.tf
@@ -1,3 +1,29 @@
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = var.default_tags
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "virginia"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
 # EKS Cluster
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
