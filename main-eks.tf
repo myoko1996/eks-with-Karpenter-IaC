402f19b9718b9793408e2e5e7b3b2436bbc838fb
Myoko <myokoko1651996@gmail.com>
2025-03-08 02:48:39 +0630

    fix  karpenter stable version compatible with k8s 1.32

--- main-eks.tf
+++ main-eks.tf
@@ -137,7 +137,7 @@ resource "helm_release" "karpenter" {
   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
   repository_password = data.aws_ecrpublic_authorization_token.token.password
   chart               = "karpenter"
-  version             = "1.0.0"
+  version             = "1.3.0"
   wait                = false
 
   values = [
@@ -150,6 +150,7 @@ resource "helm_release" "karpenter" {
       interruptionQueue: ${module.karpenter.queue_name}
     EOT
   ]
+  depends_on = [module.karpenter]
 }
 
 
